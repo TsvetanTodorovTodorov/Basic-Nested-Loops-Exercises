498e28f80bde47c73e7d4913cf725bcc5583040d
Tsvetan Todorov <111016955+TsvetanTodorovTodorov@users.noreply.github.com>
2026-02-24 18:49:52 +0200

    this is a complete program that print movies with percentage of bought tickets for every movie and for every type of ticket

--- 06-cinema-Tickets/06-cinema-Tickets.cpp
+++ 06-cinema-Tickets/06-cinema-Tickets.cpp
@@ -9,51 +9,64 @@ int main()
 	getline(cin, command); // std?
 
 	int countTotalTickets = 0;
+	double countStandardTickets = 0.0;
+	double countStudentTickets = 0.0;
+	double countKidTickets = 0.0;
+
+	cout.setf(ios::fixed);
+	cout.precision(2);
+
 	while (command != "Finish")
 	{
-		int seats;
+		double seats;
 		cin >> seats;
 		string ticketType;
 		cin >> ticketType;
 
-		int countStandardTickets = 0;
-		int countStudentTickets = 0;
-		int countKidTickets = 0;
-		
+		double counterBoughtTicketsForMovie = 0.0;
+
 		while (ticketType != "End")
-		{			
-			countTotalTickets++;
+		{		
+			counterBoughtTicketsForMovie++;
 
 			if (ticketType == "standard")
 			{
 				countStandardTickets++;
-				seats--;
-
+				countTotalTickets++;
 			} 
 			else if (ticketType == "student")
 			{
 				countStudentTickets++;
-				seats--;
+				countTotalTickets++;
 			}
 			else if (ticketType == "kid")
 			{
 				countKidTickets++;
-				seats--;
+				countTotalTickets++;
 			}
-			if (seats == 0)
+			if (counterBoughtTicketsForMovie == seats)
 			{
 				break;
 			}
 			cin >> ticketType;
 		}
-		cout << countStandardTickets << endl;
-		cout << countStudentTickets << endl;
-		cout << countKidTickets << endl;
+
+		double percentage = (counterBoughtTicketsForMovie / seats) * 100;
+		cout << command << " - " << percentage << "% full." << endl;
 
 		cin.ignore();
 		getline(cin, command);
 	}
-		cout << countTotalTickets << endl;
+
+	double percentageStudentTickets = (countStudentTickets / countTotalTickets) * 100;
+	double percentageStandardTickets = (countStandardTickets / countTotalTickets) * 100;
+	double percentageKidTickets = (countKidTickets / countTotalTickets) * 100;
+
+	cout << "Total tickets: " << countTotalTickets << endl;
+
+	cout << percentageStudentTickets << "% student tickets." << endl;
+	cout << percentageStandardTickets << "% standard tickets." << endl;
+	cout << percentageKidTickets << "% kids tickets." << endl;
 
 	return 0;
 }
